7cdeca2d72cde506e1b57a1a982fc0658145019c
Patricia Martinkova <patriciamar@users.noreply.github.com>
2021-05-21 18:19:45 +0200

    Update Installing_packages.R

--- Installing_packages.R
+++ Installing_packages.R
@@ -1,19 +1,38 @@
 #-----------------------------------------------------------------
 # Packages for code in
-# Introduction to psychometric methods
-# in educational, psychological, and health-related measurements.
-# Using R.
+# Computational aspects of psychometric methods. With R.
 # P. Martinkova & A. Hladka
 #-----------------------------------------------------------------
 
+install.packages("bayestestR")
 install.packages("brms")
+install.packages("cowplot")
+install.packages("eRm")
+install.packages("ggforce")
 install.packages("ggplot2")
 install.packages("gtheory")
 devtools::install_github("cddesja/hemp")
 install.packages("hemp")
+install.packages("moments")
+install.packages("lavaan")
 install.packages("lme4")
+install.packages("lmerTest")
+install.packages("ltm")
 install.packages("nnet")
+install.packages("plotrix")
 install.packages("psych")
 install.packages("psychometric")
+install.packages("semPlot")
 install.packages("ShinyItemAnalysis")
+devtools::install_github("patriciamar/ShinyItemAnalysis") # newest GitHub version
+install.packages("tidyverse")
 install.packages("VGAM")
+
+# further data packages
+install.packages("EstCRM")
+install.packages("lordif")
+install.packages("sirt")
+
+# figure saving packages
+install.packages("Cairo")
+install.packages("RColorBrewer")
