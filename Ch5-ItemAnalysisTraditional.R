550fb22c086a00f1d6c879c7013dfd7b4aa851d8
Patricia Martinkova <patriciamar@users.noreply.github.com>
2022-12-22 22:01:51 +0100

    Update Ch5-ItemAnalysisTraditional.R

--- Ch5-ItemAnalysisTraditional.R
+++ Ch5-ItemAnalysisTraditional.R
@@ -5,12 +5,6 @@
 #-----------------------------------------------------------------
 
 #-----------------------------------------------------------------
-# Packages
-#-----------------------------------------------------------------
-
-library(ggplot2)
-
-#-----------------------------------------------------------------
 # Plot settings
 #-----------------------------------------------------------------
 
@@ -42,23 +36,19 @@ data(HCI, package = "ShinyItemAnalysis")
 #--------------
 # item difficulty (average item score)
 sapply(HCI[, 1:20], mean)
-## Item 1  Item 2  Item 3  Item 4  Item 5  Item 6  Item 7  Item 8
-## 0.6989  0.7527  0.8479  0.4040  0.4424  0.3625  0.5469  0.7051
-## Item 9 Item 10 Item 11 Item 12 Item 13 Item 14 Item 15 Item 16
-## 0.4332  0.6482  0.7788  0.5730  0.6083  0.7588  0.4439  0.6068
-## Item 17 Item 18 Item 19 Item 20
-## 0.2965  0.7972  0.7849  0.7220
+##  Item 1  Item 2  Item 3  Item 4  Item 5  Item 6  Item 7  Item 8  Item 9 Item 10 
+##  0.6989  0.7527  0.8479  0.4040  0.4424  0.3625  0.5469  0.7051  0.4332  0.6482 
+## Item 11 Item 12 Item 13 Item 14 Item 15 Item 16 Item 17 Item 18 Item 19 Item 20 
+##  0.7788  0.5730  0.6083  0.7588  0.4439  0.6068  0.2965  0.7972  0.7849  0.7220 
 #--------------
 
 #--------------
 # item standard deviation
 sapply(HCI[, 1:20], sd)
-## Item 1  Item 2  Item 3  Item 4  Item 5  Item 6  Item 7  Item 8
-## 0.4591  0.4318  0.3594  0.4911  0.4971  0.4811  0.4982  0.4564
-## Item 9 Item 10 Item 11 Item 12 Item 13 Item 14 Item 15 Item 16
-## 0.4959  0.4779  0.4154  0.4950  0.4885  0.4281  0.4972  0.4888
-## Item 17 Item 18 Item 19 Item 20
-## 0.4571  0.4024  0.4112  0.4484
+##  Item 1  Item 2  Item 3  Item 4  Item 5  Item 6  Item 7  Item 8  Item 9 Item 10 
+##  0.4591  0.4318  0.3594  0.4911  0.4971  0.4811  0.4982  0.4564  0.4959  0.4779 
+## Item 11 Item 12 Item 13 Item 14 Item 15 Item 16 Item 17 Item 18 Item 19 Item 20 
+##  0.4154  0.4950  0.4885  0.4281  0.4972  0.4888  0.4571  0.4024  0.4112  0.4484 
 #--------------
 
 #-----------------------------------------------------------------
@@ -94,9 +84,8 @@ mean(Anxiety[, "R25"] >= 2)
 #--------------
 library(ShinyItemAnalysis)
 Anxiety_items <- Anxiety[, paste0("R", 1:29)]
-ItemAnalysis(Data = Anxiety_items)[, c("Difficulty", "Mean", "SD", 
-                                       "Min.score", "Max.score", 
-                                       "Prop.max.score")]
+ItemAnalysis(Data = Anxiety_items)[, c("Difficulty", "Mean", "SD",  "Min.score", 
+                                       "Max.score", "Prop.max.score")]
 ##      Difficulty  Mean      SD Min.score Max.score Prop.max.score
 ## R1       0.1230 1.4922 0.8303         1         5         0.0078
 ## ...
@@ -150,35 +139,30 @@ ItemAnalysis(HCI[, 1:20])$RIR
 
 #--------------
 gDiscrim(Data = HCI[, 1:20])
-## Item 1  Item 2  Item 3  Item 4  Item 5  Item 6  Item 7  Item 8
-## 0.4194  0.3041  0.3272  0.3088  0.4562  0.5069  0.2350  0.4470
-## Item 9 Item 10 Item 11 Item 12 Item 13 Item 14 Item 15 Item 16
-## 0.3548  0.4608  0.3318  0.4793  0.5438  0.4378  0.4885  0.5576
-## Item 17 Item 18 Item 19 Item 20
-## 0.1659  0.4286  0.4147  0.4562
+##  Item 1  Item 2  Item 3  Item 4  Item 5  Item 6  Item 7  Item 8  Item 9 Item 10 
+##  0.4580  0.3823  0.3832  0.3006  0.4633  0.4935  0.2440  0.4744  0.3439  0.3911 
+## Item 11 Item 12 Item 13 Item 14 Item 15 Item 16 Item 17 Item 18 Item 19 Item 20 
+##  0.3905  0.5162  0.5298  0.4962  0.4660  0.5972  0.1386  0.5209  0.5253  0.4596 
 #--------------
 
 #--------------
 gDiscrim(Data = HCI[, 1:20], k = 5, l = 4, u = 5)
-## Item 1  Item 2  Item 3  Item 4  Item 5  Item 6  Item 7  Item 8
-## 0.07692 0.07692 0.01538 0.27692 0.19231 0.15385 0.19231 0.12308
-## Item 9 Item 10 Item 11 Item 12 Item 13 Item 14 Item 15 Item 16
-## 0.30000 0.15385 0.06923 0.13846 0.11538 0.03077 0.23077 0.07692
-## Item 17 Item 18 Item 19 Item 20
-## 0.24615 0.02308 0.05385 0.08462
+##  Item 1  Item 2  Item 3  Item 4  Item 5  Item 6  Item 7  Item 8  Item 9 Item 10 
+##  0.0783  0.0614  0.0085  0.2693  0.1778  0.1423  0.1942  0.1228  0.2889  0.1402 
+## Item 11 Item 12 Item 13 Item 14 Item 15 Item 16 Item 17 Item 18 Item 19 Item 20 
+##  0.0667  0.1434  0.1153  0.0265  0.1979  0.0778  0.2503  0.0180  0.0497  0.0836
 #--------------
 
 #--------------
 # Summary table (not shown in the book)
 # ItemAnalysis() with all above indices
 ItemAnalysis(Data = HCI[, 1:20], k = 5, l = 4, u = 5)[
-  , c("Difficulty", "SD", "ULI", "gULI", "RIT", "RIR")
-]
-##        Difficulty     SD    ULI    gULI    RIT    RIR
-## Item 1     0.6989 0.4591 0.4194 0.07692 0.4019 0.2884
-## Item 2     0.7527 0.4318 0.3041 0.07692 0.3320 0.2206
+  , c("Difficulty", "SD", "ULI", "gULI", "RIT", "RIR")]
+##        Difficulty     SD    ULI   gULI    RIT    RIR
+## Item 1     0.6989 0.4591 0.4580 0.0783 0.4019 0.2884
+## Item 2     0.7527 0.4318 0.3823 0.0614 0.3320 0.2206
 ## ...
-## Item 20    0.7220 0.4484 0.4562 0.08462 0.4463 0.3396
+## Item 20    0.7220 0.4484 0.4596 0.0836 0.4463 0.3396
 #--------------
 
 #--------------
@@ -186,8 +170,7 @@ DDplot(Data = HCI[, 1:20], discrim = "ULI")
 #--------------
 
 #--------------
-DDplot(Data = HCI[, 1:20], discrim = "ULI", 
-       k = 5, l = 4, u = 5, thr = 0.1)
+DDplot(Data = HCI[, 1:20], discrim = "ULI", k = 5, l = 4, u = 5, thr = 0.1)
 #--------------
 
 #-----------------------------------------------------------------
@@ -196,8 +179,8 @@ DDplot(Data = HCI[, 1:20], discrim = "ULI",
 
 #--------------
 data(HCIkey, HCItest, package = "ShinyItemAnalysis")
-DistractorAnalysis(Data = HCItest[, 1:20], key = HCIkey, 
-                   item = c(3, 17), num.group = 5)
+DistractorAnalysis(Data = HCItest[, 1:20], key = HCIkey, item = c(3, 17), 
+                   num.group = 5)
 ## $`Item 3`
 ## score.level
 ## response Group1 Group2 Group3 Group4 Group5
@@ -215,9 +198,8 @@ DistractorAnalysis(Data = HCItest[, 1:20], key = HCIkey,
 #--------------
 
 #--------------
-plotDistractorAnalysis(Data = HCItest[, 1:20], key = HCIkey, 
-                       num.group = 5, item = c(3, 17), 
-                       multiple.answers = TRUE)
+plotDistractorAnalysis(Data = HCItest[, 1:20], key = HCIkey, item = c(3, 17), 
+                       num.group = 5, multiple.answers = TRUE)
 #--------------
 
 #-----------------------------------------------------------------
@@ -229,23 +211,20 @@ psych::alpha(x = HCI[, 1:20])$total[1]
 #  raw_alpha
 #  0.7153
 psych::alpha(x = HCI[, 1:20])$alpha.drop[, 1]
-##  [1] 0.7042 0.7099 0.7007 0.7151 0.7054 0.6991 0.7220 0.7009 0.7114
-## [10] 0.7064 0.7047 0.7016 0.6972 0.7001 0.7030 0.6952 0.7253 0.6943
-## [19] 0.6981 0.6997
+##  [1] 0.7042 0.7099 0.7007 0.7151 0.7054 0.6991 0.7220 0.7009 0.7114 0.7064
+## [11] 0.7047 0.7016 0.6972 0.7001 0.7030 0.6952 0.7253 0.6943 0.6981 0.6997
 #--------------
 
 #--------------
 ItemAnalysis(Data = HCI[, 1:20])$Alpha.drop
-##  [1] 0.7042 0.7099 0.7007 0.7151 0.7054 0.6991 0.7220 0.7009 0.7114
-## [10] 0.7064 0.7047 0.7016 0.6972 0.7001 0.7030 0.6952 0.7253 0.6943
-## [19] 0.6981 0.6997
+##  [1] 0.7042 0.7099 0.7007 0.7151 0.7054 0.6991 0.7220 0.7009 0.7114 0.7064
+## [11] 0.7047 0.7016 0.6972 0.7001 0.7030 0.6952 0.7253 0.6943 0.6981 0.6997
 #--------------
 
 #--------------
 ItemAnalysis(Data = HCI[, 1:20], criterion = HCI$major)[, "Index.rel"]
-## [1]  0.1844 0.1432 0.1563 0.1484 0.1989 0.2192 0.1172 0.1984 0.1690
-## [10] 0.1835 0.1611 0.2154 0.2319 0.1885 0.2098 0.2413 0.0769 0.2036
-## [19] 0.1899 0.1999
+##  [1] 0.1845 0.1434 0.1564 0.1485 0.1991 0.2194 0.1173 0.1985 0.1692 0.1837
+## [11] 0.1613 0.2155 0.2321 0.1886 0.2100 0.2415 0.0770 0.2037 0.1901 0.2001
 #--------------
 
 #-----------------------------------------------------------------
@@ -254,9 +233,8 @@ ItemAnalysis(Data = HCI[, 1:20], criterion = HCI$major)[, "Index.rel"]
 
 #--------------
 ItemAnalysis(Data = HCI[, 1:20], criterion = HCI$major)$Corr.criterion
-##  [1] 0.1173  0.1265  0.1541  0.0832  0.1274  0.0980 -0.0194  0.0949
-##  [9] 0.0681  0.0968  0.0782  0.0938  0.0909  0.0664  0.0859  0.0499
-## [17] 0.0312  0.1187  0.1295  0.0650
+##  [1] 0.1173 0.1265 0.1541 0.0832 0.1274 0.0980 -0.0194 0.0949 0.0681 0.0968
+## [11] 0.0782 0.0938 0.0909 0.0664 0.0859 0.0499  0.0312 0.1187 0.1295 0.0650
 #--------------
 
 #--------------
@@ -284,23 +262,36 @@ length(HeightInventory[, 1])
 
 #--------------
 ItemAnalysis(Data = HeightInventory[, 1:26])$"Perc.miss"
-##  [1] 0.1842 1.4125 0.6141 0.1024 0.1433 0.2047 0.0614 0.2047
-##  [9] 0.1638 0.1433 0.0614 0.0819 0.2252 1.5353 0.1842 0.1638
-## [17] 0.4504 0.0409 0.1228 0.1024 0.1638 0.1638 0.1638 0.1433
-## [25] 0.0819 0.1228
+##  [1] 0.1842 1.4125 0.6141 0.1024 0.1433 0.2047 0.0614 0.2047 0.1638 0.1433
+## [11] 0.0614 0.0819 0.2252 1.5353 0.1842 0.1638 0.4504 0.0409 0.1228 0.1024
+## [21] 0.1638 0.1638 0.1638 0.1433 0.0819 0.1228
 #--------------
 
 #--------------
-# Table of missingness in Item 2 vs gender (not shown in book)
+# table of missingness in Item 2 vs gender (not shown in book)
 table(is.na(HeightInventory[, 2]), HeightInventory[,28])
 #--------------
 
 #--------------
 ItemAnalysis(Data = HeightInventory[, 1:26])$"Perc.nr"
-##  [1] 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000
-##  [9] 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000
-## [17] 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000
-## [25] 0.0000 0.1228
+##  [1] 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000
+## [11] 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000
+## [21] 0.0000 0.0000 0.0000 0.0000 0.0000 0.1228
+#--------------
+
+#--------------
+# All indices (code not shown in the book)
+ItemAnalysis(Data = HeightInventory[, 1:26], minscore = 1, maxscore = 4, 
+             criterion = HeightInventory$"HeightCM")[1:2, ]
+##               Difficulty Mean    SD Cut.score obs.min Min.score obs.max Max.score
+## ShortTrousers      0.312 1.94 0.892        NA       1         1       4         4
+## TallerThanM        0.303 1.91 0.808        NA       1         1       4         4
+##               Prop.max.score   RIR   RIT Corr.criterion   ULI gULI Alpha.drop Index.rel
+## ShortTrousers          0.073 0.685 0.713          0.577 0.482   NA      0.966     0.636
+## TallerThanM            0.039 0.762 0.782          0.758 0.470   NA      0.966     0.632
+##               Index.val Perc.miss Perc.nr
+## ShortTrousers     0.515     0.184       0
+## TallerThanM       0.612     1.412       0
 #--------------
 
 #-----------------------------------------------------------------
@@ -308,6 +299,3 @@ ItemAnalysis(Data = HeightInventory[, 1:26])$"Perc.nr"
 #-----------------------------------------------------------------
 
 startShinyItemAnalysis()
-
-
-
