ea54f156fb3fc3cb5e2f21ec8d820f159487e8cd
Patricia Martinkova <patriciamar@users.noreply.github.com>
2022-12-22 21:19:56 +0100

    Update InstallPackages.R

--- InstallPackages.R
+++ InstallPackages.R
@@ -1,25 +1,22 @@
 #-----------------------------------------------------------------
-# Packages for code in
 # Computational aspects of psychometric methods. With R.
 # P. Martinkova & A. Hladka
+# 
+# Code for installation of packages
 #-----------------------------------------------------------------
 
 install.packages("remotes")
 
 install.packages("aod")
-install.packages("bayestestR")
 install.packages("brms")
-install.packages("Cairo")
-install.packages("corrplot")
-install.packages("cowplot")
+install.packages("catR")
 install.packages("deltaPlotR")
 install.packages("DFIT")
 install.packages("difNLR")
 install.packages("difR")
+install.packages("equateIRT")
 install.packages("eRm")
-install.packages("EstCRM") # data EPIA
 install.packages("ggdendro")
-install.packages("ggforce")
 install.packages("ggplot2")
 install.packages("GPArotation")
 install.packages("gtheory")
@@ -27,29 +24,23 @@ install.packages("hemp")
 install.packages("lattice")
 install.packages("lavaan")
 install.packages("lme4")
-install.packages("lmerTest")
 install.packages("lordif")
 install.packages("ltm")
 install.packages("mirt")
 install.packages("mirtCAT")
 install.packages("moments")
 install.packages("msm")
-install.packages("naniar")
+install.packages("nlme")
 install.packages("nnet")
-install.packages("plotrix")
 install.packages("psych")
 install.packages("psychometric")
-install.packages("RColorBrewer")
-install.packages("reshape")
 install.packages("semPlot")
 install.packages("ShinyItemAnalysis", dependencies = TRUE)
-install.packages("sirt") # data rating
 install.packages("TAM")
 install.packages("tidyverse")
 install.packages("VGAM")
 
 # install from GitHub (packages not available on CRAN, or newest versions)
 remotes::install_github("adelahladka/difNLR")  # newest GitHub version
-# remotes::install_github("coolbutuseless/ggpattern")
 remotes::install_github("cddesja/hemp")
 remotes::install_github("patriciamar/ShinyItemAnalysis") # newest GitHub version
